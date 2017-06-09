7179e3956c25ab338757eae8b6a0ec51952c28c1
VaneGarciaYara <vanegarciay@gmail.com>
2017-06-08 22:38:49 -0400

    arreglo de menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,22 @@ document.addEventListener("scroll", ajustarMenu);
 function ajustarMenu() {
     var nav = document.getElementsByTagName("nav")[0];
     var logo = document.getElementById("logo");
+    var login = document.getElementById("login");
+    var menu = document.getElementById("menu");
+    var sign;
+    var signUp;
 
     if (document.body.scrollTop > 100) {
-        nav.classList.add("nav-blanco");
-        logo.setAttribute("src", "assets/img/logo-pink.png");
+        if(document.getElementsByClassName("nav-blanco")[0] == null) {
+            nav.classList.add("nav-blanco");
+            logo.setAttribute("src", "assets/img/logo-pink.png");
+            document.getElementById("sign").classList.toggle("hidden");
+      }
     } else {
-        nav.classList.remove("nav-blanco");
-        logo.setAttribute("src", "assets/img/logo-white.png");
+        if(document.getElementsByClassName("nav-blanco")[0] != null) {
+            nav.classList.remove("nav-blanco");
+            logo.setAttribute("src", "assets/img/logo-white.png");
+            document.getElementById("sign").classList.toggle("hidden");
+        }
     }
-}
+} 
